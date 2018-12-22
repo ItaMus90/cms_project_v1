25c08691caab386d86f71f156468aa8c853d629d
ItaMus90 <itamarmuslvi@gmail.com>
2018-12-22 17:26:26 +0200

    add config db file

---
+++ includes/db.php
@@ -0,0 +1,22 @@
+<?php
+
+    $db["db_host"] = "localhost";
+    $db["db_username"] = "root";
+    $db["db_password"] = "";
+    $db["db_name"] = "cms_v1";
+
+
+    foreach($db as $key => $value){
+
+        define(strtoupper($key), $value);
+
+    }
+    
+
+    $connection =  mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+    if($connection){
+
+        echo "Ok";
+
+    }
