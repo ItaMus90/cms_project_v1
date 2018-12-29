50fb8ea35600e9cbb8bfdb8f9363b29c3088c52a
ItaMus90 <itamarmuslvi@gmail.com>
2018-12-29 16:24:50 +0200

    build search engine part 3

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,33 +1,10 @@
 <div class="col-md-4">
 
 
-<?php
-
-    if (isset($_POST["submit"])){
-
-        $search = $_POST["search"];
-    
-        $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
-        $search_query = mysqli_query($connection, $query);
-
-        if (!$search_query)
-            die("QUERY FAILED " .mysqli_error($connection));
-
-
-        $count = mysqli_num_rows($search_query);
-
-        if ($count === 0) {
-            echo "<h1>NO RESULT</h1>";
-        }
-    }
-
-?>
-
-
 <!-- Blog Search Well -->
 <div class="well">
     <h4>Blog Search</h4>
-    <form method="post" >
+    <form method="post" action="search.php">
         <div class="input-group">
             <input name="search" type="text" class="form-control">
             <span class="input-group-btn">
