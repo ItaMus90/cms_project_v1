b4981273a72dfb8e377b279cf1b1ff5fd28adb62
ItaMus90 <itamarmuslvi@gmail.com>
2018-12-29 15:42:23 +0200

    add data to db and pop them to the client

--- includes/db.php
+++ includes/db.php
@@ -17,6 +17,6 @@
 
     if($connection){
 
-        echo "Ok";
+       
 
     }
--- includes/navigation.php
+++ includes/navigation.php
@@ -13,15 +13,23 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
+
+
+                    <?php 
+                    
+                        $query = "SELECT category_title FROM categories";
+                        $query_select_category_title = mysqli_query($connection, $query);
+
+
+                        while($row = mysqli_fetch_assoc($query_select_category_title)){
+
+                            $cat_title = $row["category_title"];
+
+                            echo "<li> <a href='#'>" . $cat_title . "</a></li>";
+
+                        }
+                    
+                    ?>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
